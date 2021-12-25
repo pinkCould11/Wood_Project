3f54ad3da506d60fd1755db748a9664131e4366d
Hana <jlucy0411@gmail.com>
2021-12-25 17:58:41 -0600

    Initial Commit

---
+++ Wood_Project/Wood_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wood_ProjectApp.swift
+//  Wood_Project
+//
+//  Created by Hana Keinan on 12/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Wood_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
